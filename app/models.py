829154afcd2e2c8a716a66c6a753fbec6d0ddea0
Cert Manager <deploy@certmanager>
2026-08-11 20:36:03 +0000

    fix: datetime formatting em templates e models

--- app/models.py
+++ app/models.py
@@ -7,8 +7,7 @@ from flask import current_app, g
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
+            current_app.config['DATABASE']
         )
         g.db.row_factory = sqlite3.Row
     return g.db
